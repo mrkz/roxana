38f496411b75b1b1ef781fa3637f74261b6cb7a0
Marcos Eleno Joaquín Simental Magaña <mrkzmrkz@gmail.com>
2014-02-27 15:50:13 -0600

    Añado comparacion de puntos por medio del Ángulo entre punto 0 y punto K-ésimo respecto al eje X.

--- src/com/algoritmia/Algoritmos.java
+++ src/com/algoritmia/Algoritmos.java
@@ -44,6 +44,28 @@ public class Algoritmos
 		return listaPuntos;
 	}
 	
+	/**
+	 * compara angulos de p1 y p2 respecto a p0
+	 * regresa:
+	 *  -1 si angulo p1 < p2
+	 *  0  si p1 = p2
+	 *  1  si p1 > p2 
+	 */
+	public static int puntoMenorRespectoP0(Punto p0, Punto p1, Punto p2){
+		double p1Cot, p2Cot;
+		int deltaX, deltaY;
+		
+		deltaX = p1.getX() - p0.getX();
+		deltaY = p1.getY() - p0.getY();
+		p1Cot  = Math.atan2(deltaY, deltaX);
+		deltaX = p2.getX() - p0.getX();
+		deltaY = p2.getY() - p0.getY();
+		p2Cot  = Math.atan2(deltaY, deltaX);
+		if(p1Cot < p2Cot) return -1;
+		else if(p1Cot > p2Cot) return 1;
+		return 0;
+	}
+	
 	public static Punto damePuntoMasBajo(ArrayList<Punto> listaPuntos){
 		Punto p0, puntoN;
 		Iterator<Punto> iter = listaPuntos.iterator();
